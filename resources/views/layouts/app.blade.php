3f8c57268ece3f10e55e93e86ba0a938b8c671a1
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2022-08-22 13:58:03 +0700

    Fix base url and add about page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,8 +31,8 @@
         <nav
             class="grid place-items-center bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div class="container grid grid-cols-4">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
+                <a href="{{ config('app.url') }}" class="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Rafflin Logo">
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ config('app.name') }}</span>
                 </a>
                 <!-- <div class="flex md:order-2">
@@ -75,12 +75,11 @@
                     <ul
                         class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <a href="#"
-                                class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                                aria-current="page">Home</a>
+                            <a href="/"
+                                class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">Home</a>
                         </li>
                         <li>
-                            <a href="#"
+                            <a href="{{ route('about') }}"
                                 class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
                         </li>
                         <li>
@@ -106,7 +105,7 @@
 
         <footer
             class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="{{ url('') }}"
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="{{ config('app.url') }}"
                     class="hover:underline">{{ config('app.name') }}</a>. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
