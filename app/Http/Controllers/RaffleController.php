5a5809af62a54f8fe9079d71f7af3d5e3ca4468a
3instein <reynaldi_kindarto@yahoo.com>
2022-08-23 16:37:02 +0700

    Initialize raffle model

---
+++ app/Http/Controllers/RaffleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRaffleRequest;
+use App\Http\Requests\UpdateRaffleRequest;
+use App\Models\Raffle;
+
+class RaffleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRaffleRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRaffleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Raffle  $raffle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Raffle $raffle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Raffle  $raffle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Raffle $raffle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRaffleRequest  $request
+     * @param  \App\Models\Raffle  $raffle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRaffleRequest $request, Raffle $raffle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Raffle  $raffle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Raffle $raffle)
+    {
+        //
+    }
+}
