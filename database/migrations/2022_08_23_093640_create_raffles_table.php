9bba7ed2b0f4d99c800d7a94708fc275e1137bd5
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2022-08-24 13:09:07 +0700

    Create raffle migration

--- database/migrations/2022_08_23_093640_create_raffles_table.php
+++ database/migrations/2022_08_23_093640_create_raffles_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('raffles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description');
+            $table->string('banner_image')->nullable();
             $table->timestamps();
         });
     }
